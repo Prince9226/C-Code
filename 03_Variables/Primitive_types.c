2bc2a5ef1c69e03cf9c72dbf8d54c12ad28168e3
Prince Yadav <py7496813@gmail.com>
2025-06-11 08:01:11 +0530

    Create Primitive_types.c

---
+++ 03_Variables/Primitive_types.c
@@ -0,0 +1,18 @@
+#include <stdbool.h>
+#include <stdio.h>
+
+int main() 
+{
+	int age = 39;
+    char name[] = "Sandeep";
+    float weight = 68.5;
+    bool isMarried = true;
+    char gender = 'M';
+  
+    printf("%d \n", age);
+	  printf("%s \n", name);
+    printf("%f \n", weight);
+    printf("%d \n", isMarried);
+    printf("%c \n", gender);
+    return 0;
+}
