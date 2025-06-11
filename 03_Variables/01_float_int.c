5e7bf51963927d1a06abc87e35690474220fdb2a
Prince Yadav <py7496813@gmail.com>
2025-06-11 07:55:21 +0530

    Create 01_float_int.c

---
+++ 03_Variables/01_float_int.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main()
+{
+  float a = 5.4;
+  printf("%f \n",a);
+
+  int num = 4;
+  printf("%d \n",num);
+
+  num = 55;
+  printf("%d \n",num);
+  return 0;
+}
+
+
+// Output
+/*
+5.400000 
+4 
+55 
+  */
