47e83d9ac5093a3f413b6671d5cdeae4bedb0d59
Prince Yadav <py7496813@gmail.com>
2025-06-11 07:38:11 +0530

    Create 02_comment.c

---
+++ 02_comment.c
@@ -0,0 +1,11 @@
+/* This is an example of
+multiline comment */
+
+#include <stdio.h>
+int main() {
+
+  	// Example of single line comment
+  
+	printf("Hey! My name is Prince Yadav");
+    return 0;
+}
