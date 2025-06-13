3d996d1e0d6a1ce704b219d94ba1a2690a9a39c1
Prince Yadav <py7496813@gmail.com>
2025-06-13 16:54:08 +0530

    Create 02_float.c

---
+++ 03_Variables/format_specifier/02_float.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main()
+{
+    float x = 15.25;
+    double y = 120.387648;
+    double z = 20E+4;
+
+    printf("%f\n%f\n%f\n", x, y, z);
+    return 0;
+}
+
+/* output
+15.250000
+120.387648
+200000.000000
+  */
