054a04afd0a0b1e00e8abbf74cd4f873ecf8da6a
Prince Yadav <py7496813@gmail.com>
2025-06-13 16:37:42 +0530

    Create 08_scanf.c

---
+++ 03_Variables/08_scanf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main()
+{
+  	char name[100];
+  	printf("Enter Your Name: \n");
+  	scanf("%s", name);
+  
+  	printf("Hi %s,\n", name);
+  	printf("Wecome to GitHub!");
+    return 0;
+}
