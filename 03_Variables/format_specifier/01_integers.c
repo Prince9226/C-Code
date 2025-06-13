6a8fa1d5c7c2f513076be826d269c8e77c637531
Prince Yadav <py7496813@gmail.com>
2025-06-13 16:51:31 +0530

    Create 01_integers.c

---
+++ 03_Variables/format_specifier/01_integers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+    int a = 10;     // signed integer
+    printf("%d \n", a);
+
+    long b = 20;    // long integar
+    printf("%ld \n", b);
+
+    unsigned c = 30;     // unsigned integer
+    printf("%u \n", c);
+
+    long long d = 40;     // long integar
+    printf("%lld \n", d);
+
+    return 0;
+}
