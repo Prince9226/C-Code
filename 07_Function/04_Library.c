8d7791b803817e10ee61063c69feb21e44e22bf7
Prince Yadav <py7496813@gmail.com>
2025-06-14 08:01:33 +0530

    Create 04_Library.c

---
+++ 07_Function/04_Library.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+#include<math.h>
+int main ()
+{
+    int a = 10, b = 3, c = 100;      
+    printf("%.2f\n",pow(a,b));      // 100.00
+    printf("%.2f\n",log10(c));       // 2.00
+    printf("%.2f\n",sqrt(c));      // 10.00
+    printf("%f\n",exp(b));        // 20.085537
+}
