a0b2274252eb7d19a316f24aff8265d0fc13ea92
Prince Yadav <py7496813@gmail.com>
2025-06-14 08:46:33 +0530

    Create 06_practice.c

---
+++ 07_Function/06_practice.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+void fun()
+{
+    static int x = 0;
+    int y = 0;
+    x++;
+    y++;
+    printf("%d %d\n",x,y);
+}
+int main(){
+    fun();
+    fun();
+    return 0;
+}
+
+/* Output
+1 1
+1 2
+*/
