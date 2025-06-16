e9061643fcbb57bace736c8817cffb2888da6a97
Prince Yadav <py7496813@gmail.com>
2025-06-16 20:34:28 +0530

    Create 02_nested_if_else_statement.c

---
+++ 06_Control_statement/02_nested_if_else_statement.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+int main()
+{
+    int x;
+    printf("Enter a Number: \n");
+    scanf("%d", &x);
+    if (x > 0)
+    {
+        printf("Positive ");
+        if (x % 2 == 0)
+            printf("Even\n");
+        else
+            printf("Odd\n");
+    }
+    else if (x < 0)
+    {
+        printf("Negative ");
+        if (x % 2 == 0)
+            printf("Even\n");
+        else
+            printf("Odd\n");
+    }
+    else
+    {
+        printf("Zero\n");
+    }
+    return 0;
+}
