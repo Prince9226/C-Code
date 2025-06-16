5e354a2358da0438f11ae51431054a5e2ce9042a
Prince Yadav <py7496813@gmail.com>
2025-06-16 20:00:44 +0530

    Create 09_operator_sizeof.c

---
+++ 05_Operators/09_operator_sizeof.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main()
+{
+	printf("%zu \n", sizeof(int));
+	printf("%zu \n", sizeof(char));
+	printf("%zu \n", sizeof(long long));
+	printf("%zu", sizeof(float));
+	return 0;
+}
