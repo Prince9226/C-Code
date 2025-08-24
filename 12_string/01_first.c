4ff51a68ac5fec940714259890a42efd5a3c0a11
Prince Yadav <py7496813@gmail.com>
2025-08-24 20:05:19 +0530

    Create 01_first.c

---
+++ 12_string/01_first.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+#include<string.h>
+int main(){
+    // Way to define string. 
+    char str1[] = "Prince Yadav";
+    char str2[12]="Abhi singh" ;
+    printf("Your first string is :%s\n",str1);
+    printf("Your second string is :%s",str2);
+    return 0;
+}
